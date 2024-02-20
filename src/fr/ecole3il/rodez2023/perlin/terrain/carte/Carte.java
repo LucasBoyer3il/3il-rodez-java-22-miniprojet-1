8de76a035799d47f5ddb7f400ed55adf53ca6dff
Lucas Boyer <boyerl@3il.fr>
2024-02-20 23:57:34 +0100

    Implémentation constructeur Carte avec donneesCarte quand on charge une carte (pas tout à fait foncionnel)

--- src/fr/ecole3il/rodez2023/perlin/terrain/carte/Carte.java
+++ src/fr/ecole3il/rodez2023/perlin/terrain/carte/Carte.java
@@ -23,18 +23,27 @@ public class Carte {
         nom = chaineCoupee[0];
         largeur = Integer.parseInt(chaineCoupee[1]);
         hauteur = Integer.parseInt(chaineCoupee[2]);
-        /*terrains = new Terrain[largeur][hauteur];
+        carte = new Terrain[largeur][hauteur];
         //lignes
-        for (int i = 3 ; i < chaineCoupee[i].length() ; i++) {
+        int x = 0;
+        int y = 0;
+        for (int i = 1 ; i < hauteur; i++) {
             //colonnes
-            for (int j = 0 ; j < chaineCoupee[j].length() ; j = j + 14) {
-                //TODO : implémenter le signe négatif
-                double altitude = Double.parseDouble(chaineCoupee[j].substring(j, j + 4));
-                double hydrometrie = Double.parseDouble(chaineCoupee[j].substring(j + 5, j + 9));
-                double temperature = Double.parseDouble(chaineCoupee[j].substring(j + 10, j + 14));
-                terrains[i][j] = new Terrain(altitude, hydrometrie, temperature);
+            String[] chaineCoupeeColonnes = chaineCoupee[i + 3].split(" ");
+            for (int j = 0 ; j < chaineCoupeeColonnes.length ; j++){
+                double altitude = Double.parseDouble(chaineCoupeeColonnes[0]);
+                double hydrometrie = Double.parseDouble(chaineCoupeeColonnes[1]);
+                double temperature = Double.parseDouble(chaineCoupeeColonnes[2]);
+                carte[x][y] = new Terrain(altitude, hydrometrie, temperature);
+                if (y == largeur - 1) {
+                    x++;
+                    y = 0;
+                } else {
+                    y++;
+                }
+
             }
-        }*/
+        }
     }
 
     public String getNom() {
